5a58d8af3d065362546b5afff09a78d9a7713e97
giang <giang8692@gmail.com>
2025-03-02 23:25:52 +0700

    Add: Model and Migration

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Option extends Model
 {
     public function poll(): BelongsTo
     {
         return $this->belongsTo(Poll::class);
+    }
+
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
     }
 }
--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vote extends Model
 {
-    //
+    public function option(): BelongsTo
+    {
+        return $this->belongsTo(Option::class);
+    }
 }
