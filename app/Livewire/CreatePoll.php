50b5a1d2f97ae0b2a3e86b2970464be02fab06fe
giang <giang8692@gmail.com>
2025-03-03 14:33:40 +0700

    Add: Create Poll with livewire

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
 
 class CreatePoll extends Component
@@ -9,8 +10,35 @@ class CreatePoll extends Component
 
     public $title;
 
+    public $options = [""];
+
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
+
+    public function createPoll()
+    {
+       Poll::create([
+            'title' => $this->title,
+        ])->options()->createManu(
+            collect($this->options)
+            ->map(fn($option) => ['name' => $option])
+            ->all()
+        );
+
+        $this->reset('title', 'options');
+
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,7 +1,23 @@
 <div>
-    <form>
+    <form wire:submit.prevent="createPoll">
+        <h1>Create New Poll</h1>
         <label>Poll Title</label>
         <input type="text" wire:model.live='title' />
-        <p>Current title: {{ $title }}</p>
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+
+        </div>
+
+        <div>
+            @foreach ($options as $index => $option)
+                <label>Option {{ $index + 1 }}</label>
+                <input type="text" wire:model.live='options.{{ $index }}' placeholder="Fill your option here"/>
+                <button class="btn mt-4" wire:click.prevent="removeOption({{ $index }})">Remove Option</button>
+            @endforeach
+        </div>
+        <div class="mt-4">
+            <button class="btn" type="submit">Create Poll</button>
+        </div>
+
     </form>
 </div>
